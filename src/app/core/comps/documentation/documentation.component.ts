fad7cbba83229bbd9bdde9c16c7aa380e85f0534
Bob Rinehart <rinebob111185@gmail.com>
2024-05-11 10:52:50 -0700

    [scaffolding] 1 - Scaffolding for core and features/rs components and services

---
+++ src/app/core/comps/documentation/documentation.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'rs-documentation',
+  standalone: true,
+  imports: [],
+  templateUrl: './documentation.component.html',
+  styleUrl: './documentation.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DocumentationComponent {
+
+}
---
+++ src/app/core/comps/signup/signup.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'rs-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SignupComponent {
+
+}
