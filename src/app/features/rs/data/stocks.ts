fd482d0b7a834cf9966b6fe9a30a244f94733431
rinebob <rinebob111185@gmail.com>
2024-05-19 10:52:16 -0700

    [store] 3 - RsCalcStore - implements methods; Dashboard - migrates relative strength calculation logic to utility functions; Obtains relative strength data in html directly from RsCalcStore

--- src/app/features/rs/data/stocks.ts
+++ src/app/features/rs/data/stocks.ts
@@ -4,5 +4,6 @@ import { MSFT_DATA } from "./MSFT";
 import { NVDA_DATA } from "./NVDA";
 import { QQQ_DATA } from "./QQQ";
 import { TSLA_DATA } from "./TSLA";
+import { StockData } from "../common/interfaces-rs";
 
-export  { AMAT_DATA, AAPL_DATA, MSFT_DATA, NVDA_DATA, QQQ_DATA, TSLA_DATA };
+export const ALL_STOCK_DATA: StockData[] = [AMAT_DATA, AAPL_DATA, MSFT_DATA, NVDA_DATA, QQQ_DATA, TSLA_DATA]
--- src/app/features/rs/store/rs-calcs.store.ts
+++ src/app/features/rs/store/rs-calcs.store.ts
@@ -1,16 +1,26 @@
-import { signalStore, withState, withMethods, withComputed } from "@ngrx/signals";
+import { signalStore, withState, withMethods, withComputed, patchState } from "@ngrx/signals";
+import { DataSet, RelStrTableData } from "../common/interfaces-rs";
 
 type RelStrCalcState = {
-
+    allData: DataSet,
+    relStrTableData: RelStrTableData,
 }
 
 const initialState: RelStrCalcState = {
-
+    allData: {},
+    relStrTableData: {
+		symbols: [],
+		dates: [],
+		data: [[]],
+	},
 }
 
 export const RsCalcsStore = signalStore(
     { providedIn: 'root'},
     withState(initialState),
-    withMethods((store) => ({})),
+    withMethods((store) => ({
+        setAllData(allData: DataSet){patchState(store, {allData})},
+        setRelStrTableData(relStrTableData: RelStrTableData){patchState(store, {relStrTableData})},
+    })),
     withComputed(() => ({})),
 );
