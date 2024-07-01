bba4fe6b74aed1667a6e44d26a6b723f9a9fb614
rinebob <rinebob111185@gmail.com>
2024-07-01 16:01:45 -0700

    [ui] 6 - Implements Header component; Inserts Header component in Core component

---
+++ src/app/core/comps/header/header.component.ts
@@ -0,0 +1,40 @@
+import { ChangeDetectionStrategy, Component, inject, output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { NAV_MENU_ITEMS } from '../../common/constants';
+import { Router, RouterModule } from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'rs-header',
+  standalone: true,
+  imports: [MatIconModule, MatMenuModule, RouterModule, UpperCasePipe],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+
+    openSidenav = output<boolean>();
+
+    router = inject(Router);
+
+    readonly NAV_MENU_ITEMS = NAV_MENU_ITEMS;
+
+    globalTopnavMenuCssClass = 'global-topnav-menu-css';
+
+    handleTopnavMenuOpen() {
+        // console.log('lP hTMO handle global topnav menu open called');
+    }
+
+    handleMenuOpen() {
+        // console.log('nH hMO handle menu open called');
+        this.openSidenav.emit(true);
+    }
+    
+    handleNavigation(href: string) {
+        // console.log('nH hN handle navigation called. href: ', href);
+        this.router.navigate([href]);
+
+    }
+}
