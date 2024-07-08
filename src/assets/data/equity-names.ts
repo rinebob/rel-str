257c90a55ebc427950704a6901c9f791a652676e
rinebob <rinebob111185@gmail.com>
2024-07-08 16:05:28 -0700

    [data] 2 - Adds stock symbol and company name hard-coded data set; Adds supporting interfaces

---
+++ src/assets/data/equity-names.ts
@@ -0,0 +1,410 @@
+import { Equity } from "../../app/core/common/interfaces";
+
+export const EQUITIES: Equity[] = [
+    {
+        symbol: "AAPL",
+        company: "Apple Inc."
+    },
+    {
+        symbol: "ABBV",
+        company: "AbbVie Inc."
+    },
+    {
+        symbol: "ABT",
+        company: "Abbott Laboratories"
+    },
+    {
+        symbol: "ACN",
+        company: "Accenture"
+    },
+    {
+        symbol: "ADBE",
+        company: "Adobe Inc."
+    },
+    {
+        symbol: "AIG",
+        company: "American International Group"
+    },
+    {
+        symbol: "AMGN",
+        company: "Amgen Inc."
+    },
+    {
+        symbol: "AMT",
+        company: "American Tower"
+    },
+    {
+        symbol: "AMZN",
+        company: "Amazon.com"
+    },
+    {
+        symbol: "AVGO",
+        company: "Broadcom Inc."
+    },
+    {
+        symbol: "AXP",
+        company: "American Express"
+    },
+    {
+        symbol: "BA",
+        company: "Boeing Co."
+    },
+    {
+        symbol: "BAC",
+        company: "Bank of America"
+    },
+    {
+        symbol: "BIIB",
+        company: "Biogen"
+    },
+    {
+        symbol: "BK",
+        company: "The Bank of New York Mellon"
+    },
+    {
+        symbol: "BKNG",
+        company: "Booking Holdings"
+    },
+    {
+        symbol: "BLK",
+        company: "BlackRock Inc."
+    },
+    {
+        symbol: "BMY",
+        company: "Bristol-Myers Squibb"
+    },
+    {
+        symbol: "BRK.B",
+        company: "Berkshire Hathaway"
+    },
+    {
+        symbol: "C",
+        company: "Citigroup Inc."
+    },
+    {
+        symbol: "CAT",
+        company: "Caterpillar Inc."
+    },
+    {
+        symbol: "CHTR",
+        company: "Charter Communications"
+    },
+    {
+        symbol: "CL",
+        company: "Colgate-Palmolive"
+    },
+    {
+        symbol: "CMCSA",
+        company: "Comcast Corp."
+    },
+    {
+        symbol: "COF",
+        company: "Capital One Financial Corp."
+    },
+    {
+        symbol: "COP",
+        company: "ConocoPhillips"
+    },
+    {
+        symbol: "COST",
+        company: "Costco Wholesale Corp."
+    },
+    {
+        symbol: "CRM",
+        company: "Salesforce"
+    },
+    {
+        symbol: "CSCO",
+        company: "Cisco Systems"
+    },
+    {
+        symbol: "CVS",
+        company: "CVS Health"
+    },
+    {
+        symbol: "CVX",
+        company: "Chevron Corporation"
+    },
+    {
+        symbol: "DD",
+        company: "DuPont de Nemours Inc."
+    },
+    {
+        symbol: "DHR",
+        company: "Danaher Corporation"
+    },
+    {
+        symbol: "DIS",
+        company: "The Walt Disney Company"
+    },
+    {
+        symbol: "DOW",
+        company: "Dow Inc."
+    },
+    {
+        symbol: "DUK",
+        company: "Duke Energy"
+    },
+    {
+        symbol: "EMR",
+        company: "Emerson Electric Co."
+    },
+    {
+        symbol: "EXC",
+        company: "Exelon"
+    },
+    {
+        symbol: "F",
+        company: "Ford Motor Company"
+    },
+    {
+        symbol: "FB",
+        company: "Facebook, Inc."
+    },
+    {
+        symbol: "FDX",
+        company: "FedEx"
+    },
+    {
+        symbol: "GD",
+        company: "General Dynamics"
+    },
+    {
+        symbol: "GE",
+        company: "General Electric"
+    },
+    {
+        symbol: "GILD",
+        company: "Gilead Sciences"
+    },
+    {
+        symbol: "GM",
+        company: "General Motors"
+    },
+    {
+        symbol: "GOOG",
+        company: "Alphabet Inc. (Class C)"
+    },
+    {
+        symbol: "GOOGL",
+        company: "Alphabet Inc. (Class A)"
+    },
+    {
+        symbol: "GS",
+        company: "Goldman Sachs"
+    },
+    {
+        symbol: "HD",
+        company: "The Home Depot"
+    },
+    {
+        symbol: "HON",
+        company: "Honeywell"
+    },
+    {
+        symbol: "IBM",
+        company: "International Business Machines"
+    },
+    {
+        symbol: "INTC",
+        company: "Intel Corp."
+    },
+    {
+        symbol: "JNJ",
+        company: "Johnson & Johnson"
+    },
+    {
+        symbol: "JPM",
+        company: "JPMorgan Chase & Co."
+    },
+    {
+        symbol: "KHC",
+        company: "Kraft Heinz"
+    },
+    {
+        symbol: "KO",
+        company: "The Coca-Cola Company"
+    },
+    {
+        symbol: "LIN",
+        company: "Linde plc"
+    },
+    {
+        symbol: "LLY",
+        company: "Eli Lilly and Company"
+    },
+    {
+        symbol: "LMT",
+        company: "Lockheed Martin"
+    },
+    {
+        symbol: "LOW",
+        company: "Lowe's"
+    },
+    {
+        symbol: "MA",
+        company: "Mastercard"
+    },
+    {
+        symbol: "MCD",
+        company: "McDonald's"
+    },
+    {
+        symbol: "MDLZ",
+        company: "Mondelēz International"
+    },
+    {
+        symbol: "MDT",
+        company: "Medtronic plc"
+    },
+    {
+        symbol: "MET",
+        company: "MetLife Inc."
+    },
+    {
+        symbol: "MMM",
+        company: "3M Company"
+    },
+    {
+        symbol: "MO",
+        company: "Altria Group"
+    },
+    {
+        symbol: "MRK",
+        company: "Merck & Co."
+    },
+    {
+        symbol: "MS",
+        company: "Morgan Stanley"
+    },
+    {
+        symbol: "MSFT",
+        company: "Microsoft"
+    },
+    {
+        symbol: "NEE",
+        company: "NextEra Energy"
+    },
+    {
+        symbol: "NFLX",
+        company: "Netflix"
+    },
+    {
+        symbol: "NKE",
+        company: "Nike, Inc."
+    },
+    {
+        symbol: "NVDA",
+        company: "Nvidia Corporation"
+    },
+    {
+        symbol: "ORCL",
+        company: "Oracle Corporation"
+    },
+    {
+        symbol: "PEP",
+        company: "PepsiCo"
+    },
+    {
+        symbol: "PFE",
+        company: "Pfizer Inc."
+    },
+    {
+        symbol: "PG",
+        company: "Procter & Gamble"
+    },
+    {
+        symbol: "PM",
+        company: "Philip Morris International"
+    },
+    {
+        symbol: "PYPL",
+        company: "PayPal"
+    },
+    {
+        symbol: "QCOM",
+        company: "Qualcomm"
+    },
+    {
+        symbol: "RTX",
+        company: "Raytheon Technologies"
+    },
+    {
+        symbol: "SBUX",
+        company: "Starbucks Corp."
+    },
+    {
+        symbol: "SO",
+        company: "Southern Company"
+    },
+    {
+        symbol: "SPG",
+        company: "Simon Property Group"
+    },
+    {
+        symbol: "T",
+        company: "AT&T Inc."
+    },
+    {
+        symbol: "TGT",
+        company: "Target Corporation"
+    },
+    {
+        symbol: "TMO",
+        company: "Thermo Fisher Scientific"
+    },
+    {
+        symbol: "TMUS",
+        company: "T-Mobile US"
+    },
+    {
+        symbol: "TSLA",
+        company: "Tesla, Inc."
+    },
+    {
+        symbol: "TXN",
+        company: "Texas Instruments"
+    },
+    {
+        symbol: "UNH",
+        company: "UnitedHealth Group"
+    },
+    {
+        symbol: "UNP",
+        company: "Union Pacific Corporation"
+    },
+    {
+        symbol: "UPS",
+        company: "United Parcel Service"
+    },
+    {
+        symbol: "USB",
+        company: "U.S. Bancorp"
+    },
+    {
+        symbol: "V",
+        company: "Visa Inc."
+    },
+    {
+        symbol: "VZ",
+        company: "Verizon Communications"
+    },
+    {
+        symbol: "WBA",
+        company: "Walgreens Boots Alliance"
+    },
+    {
+        symbol: "WFC",
+        company: "Wells Fargo"
+    },
+    {
+        symbol: "WMT",
+        company: "Walmart"
+    },
+    {
+        symbol: "XOM",
+        company: "Exxon Mobil Corp."
+    }
+
+
+];
