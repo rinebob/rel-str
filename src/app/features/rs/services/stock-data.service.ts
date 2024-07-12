d9633811651cd28730ff407d532e4bc9d784c7c4
rinebob <rinebob111185@gmail.com>
2024-07-11 17:36:31 -0700

    [store] 7 - Adds RelStrDbService and StockDataService skeletons; Extends RsAppStore to support remote data fetching and saving StockLists

--- src/app/features/rs/services/stock-data.service.ts
+++ src/app/features/rs/services/stock-data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { RAW_STOCK_DATA_BY_SYMBOL } from '../data/stocks';
 
 @Injectable({
     providedIn: 'root'
@@ -6,4 +7,15 @@ import { Injectable } from '@angular/core';
 export class StockDataService {
 
     constructor() { }
+
+    getStockDataBySymbol(symbol: string) {
+        // console.log('sDSvc gSDBS get data for symbol: ', symbol);
+        if (Object.keys(RAW_STOCK_DATA_BY_SYMBOL).includes(symbol)) {
+            // console.log('sDSvc gSDBS symbol exists. returning data');
+            return RAW_STOCK_DATA_BY_SYMBOL[symbol];
+        } else {
+            // console.log('sDSvc gSDBS symbol doesnt exist. returning empty array');
+            return [];
+        }
+    }
 }
