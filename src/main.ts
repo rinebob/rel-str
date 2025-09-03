38b82628c9583868db482b6f7a5eed52a530a2e6
rinebob <rinebob111185@gmail.com>
2025-09-02 19:54:38 -0700

    [prod] main.ts - removes outdated reference to secrets files; Fixes AppHosting build error

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-import { registerLicense } from '@syncfusion/ej2-base';
-import { SYNC_FUSION_LICENSE_KEY } from './secrets/secrets';
-
-// Registering Syncfusion<sup style="font-size:70%">&reg;</sup> license key
-registerLicense(SYNC_FUSION_LICENSE_KEY);
-
 // FOR SERVER SIDE RENDERING
 // bootstrapApplication(AppComponent, appConfig)
 //   .catch((err) => console.error(err));
