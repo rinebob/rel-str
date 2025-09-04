65cedafd51e33dc3f485cec26b6132d8ccae2d39
rinebob <rinebob111185@gmail.com>
2025-09-04 15:28:37 -0700

    admin (app): Adds production property to environment.ts; Adds environment.prod.ts file to project with firebase creds and production: true;

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-    production: false,
+    production: true,
     firebase: {
         "apiKey":"AIzaSyDiDoun2WG0xaTuXsI722-LjW9-6YBXZwc",
         "authDomain":"rel-str.firebaseapp.com",
