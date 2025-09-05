e1fcbbd58644ad8496e61ff01c3ef3f58efbd1f6
rinebob <rinebob111185@gmail.com>
2025-09-04 17:53:37 -0700

    cleanup (sync-chart-view): Deletes old SyncChartOne and SyncChartTwo components; Deletes unused core common app sidenav buttons constant

--- src/app/core/common/constants.ts
+++ src/app/core/common/constants.ts
@@ -1,20 +1,5 @@
-import { AppRoutes, AuthLevel, ButtonMetadata, NavItem } from "./interfaces";
+import { NavItem } from "./interfaces";
 
-
-export const APP_SIDENAV_BUTTONS: ButtonMetadata[] = [
-    // Externally visible routes
-    {url: AppRoutes.LOGIN, text: 'login'},
-    {url: AppRoutes.LOGOUT, text: 'logout'},
-    {url: AppRoutes.DOCUMENTATION, text: 'documentation', authLevel: AuthLevel.UNKNOWN},
-    {url: AppRoutes.CONTACT, text: 'contact', authLevel: AuthLevel.UNKNOWN},
-    {url: AppRoutes.SIGNUP, text: 'signup', authLevel: AuthLevel.UNKNOWN},
-    // {url: AppRoutes.BLOG, text: 'blog', authLevel: AuthLevel.UNKNOWN},
-
-    // Interal only routes
-    {url: AppRoutes.DASHBOARD, text: 'dashboard', authLevel: AuthLevel.USER},
-    {url: AppRoutes.CHART, text: 'chart', authLevel: AuthLevel.USER},
-    {url: AppRoutes.HISTORY, text: 'history', authLevel: AuthLevel.USER},
-];
 
 export const NAV_MENU_ITEMS: NavItem[] = [
     {
@@ -53,14 +38,6 @@ export const NAV_MENU_ITEMS: NavItem[] = [
         name: 'dashboard',
         text: 'dashboard',
         href: 'dashboard',
-        mobileOnly: false,
-        external: false,
-        target: '_self',
-    },
-    {
-        name: 'chart',
-        text: 'chart',
-        href: 'chart',
         mobileOnly: false,
         external: false,
         target: '_self',
