eb07f659d7e76f48f52a2a90c2cf20b898d3d51d
rinebob <rinebob111185@gmail.com>
2025-10-27 12:05:54 -0700

    refactor (ui): App routes - minor cleanup no functional changes;

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 
 export const APP_ROUTES: Routes = [
     {path: '', loadChildren: () => import('./core/core-routes')},
-
-  { path: '**', redirectTo: '/', pathMatch:'full'},
+    { path: '**', redirectTo: '/', pathMatch:'full'},
 ];
