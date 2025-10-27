e97325a7c53352ef2252a7fbc9f8b6c18b165a72
rinebob <rinebob111185@gmail.com>
2025-10-27 11:25:47 -0700

    feature (auth ui): Header component - Implements Auth in app with correctly wired login/logout buttons and user info display;

--- src/app/core/comps/header/header.component.ts
+++ src/app/core/comps/header/header.component.ts
@@ -3,12 +3,15 @@ import { ChangeDetectionStrategy, Component, inject, output } from '@angular/cor
 import { Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 import { NAV_MENU_ITEMS } from '../../common/constants';
+import { AuthStore } from '../../auth/auth.store';
+import { AppRoutes } from '../../common/interfaces';
 
 @Component({
     selector: 'rs-header',
-    imports: [MatIconModule, MatMenuModule, RouterModule, UpperCasePipe],
+    imports: [MatIconModule, MatMenuModule, MatButtonModule, RouterModule, UpperCasePipe],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
@@ -17,9 +20,15 @@ export class HeaderComponent {
 
     openSidenav = output<boolean>();
 
+    private readonly auth = inject(AuthStore);
     router = inject(Router);
 
     readonly NAV_MENU_ITEMS = NAV_MENU_ITEMS;
+
+    // Auth-facing signals
+    readonly user = this.auth.user;
+    readonly isAuthenticated = this.auth.isAuthenticated;
+    readonly loading = this.auth.loading;
 
     globalTopnavMenuCssClass = 'global-topnav-menu-css';
 
@@ -36,5 +45,17 @@ export class HeaderComponent {
         // console.log('nH hN handle navigation called. href: ', href);
         this.router.navigate([href]);
 
+    }
+
+    async onLogin() {
+        await this.router.navigate([`/${AppRoutes.LOGIN}`]);
+    }
+
+    async onSignup() {
+        await this.router.navigate([`/${AppRoutes.SIGNUP}`]);
+    }
+
+    async onSignOut() {
+        await this.auth.signOut();
     }
 }
