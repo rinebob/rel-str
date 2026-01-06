782cfd9240f5ab9156a82e1876cf130f861a7160
rinebob <rinebob111185@gmail.com>
2026-01-05 17:44:57 -0800

    feature (shared): Implements shared IntervalToggle component - Ui treatment to toggle D/W/M intervals app-wide where needed;

---
+++ src/app/features/shared/components/interval-toggle/interval-toggle.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { Timeframe } from '../../types/rs.interfaces';
+
+@Component({
+  selector: 'app-interval-toggle',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule],
+  templateUrl: './interval-toggle.component.html',
+  styleUrls: ['./interval-toggle.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IntervalToggleComponent {
+  readonly value = input.required<Timeframe>();
+  readonly valueChange = output<Timeframe>();
+
+  readonly Interval = Timeframe;
+
+  onSelect(interval: Timeframe): void {
+    if (interval === this.value()) {
+      return;
+    }
+
+    this.valueChange.emit(interval);
+  }
+}
