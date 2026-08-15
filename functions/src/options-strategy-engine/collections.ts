d331c94f94275f1b89e876304c6f7c358383e6f1
Bob Rinehart <rinebob111185@gmail.com>
2026-08-15 10:15:30 -0700

    114-121_BE-CONFIG-HYBRID-QUOTE-PROVIDER: Add options-rh-instrument-map Firestore collection
    - Add OPTIONS_RH_INSTRUMENT_MAP_COLLECTION constant to collections.ts
    - Provide a collection for persisting OCC-to-Robinhood-instrument map entries

--- functions/src/options-strategy-engine/collections.ts
+++ functions/src/options-strategy-engine/collections.ts
@@ -7,3 +7,4 @@
 export const OPTIONS_STRATEGY_INSTANCES_COLLECTION = 'options-strategy-instances';
 export const OPTIONS_STRATEGY_POSITIONS_COLLECTION = 'options-strategy-positions';
 export const OPTIONS_STRATEGY_STATS_COLLECTION = 'options-strategy-stats';
+export const OPTIONS_RH_INSTRUMENT_MAP_COLLECTION = 'options-rh-instrument-map';
